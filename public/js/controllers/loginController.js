155fb907120c9c00a8cf9d60c2b380f678f442e4
daedalus418 <morin.gaelle@gmail.com>
2017-01-10 19:52:14 +0100

    createAccount() deleted from loginController

--- public/js/controllers/loginController.js
+++ public/js/controllers/loginController.js
@@ -1,8 +1,7 @@
-function loginController(userService, sessionFactory, /*$timeout,*/$location, $rootScope) {
+function loginController(userService, sessionFactory, $location, $rootScope) {
 
     this.userService = userService;
     this.sessionFactory = sessionFactory;
-    // this.$timeout = $timeout;
     this.$location = $location;
     this.$rootScope = $rootScope;
 
@@ -26,36 +25,4 @@ function loginController(userService, sessionFactory, /*$timeout,*/$location, $r
             this.loginMessage.message = "Error login or password";
         });
     };
-
-    // this.createAccount = () => {
-    //     this.userService.create({
-    //         last_name: this.last_name,
-    //         first_name: this.first_name,
-    //         email: this.email,
-    //         password: this.password,
-    //         bij: this.bij,
-    //         isAdmin: this.isadmin
-    //     }).then((res) => {
-    //         this.sessionFactory.token = res.data.token;
-    //         this.sessionFactory.user = res.data.user;
-    //         this.sessionFactory.isLogged = true;
-    //         this.$rootScope.$emit('loginStatusChanged', true);
-    //         this.loginMessage = {};
-    //         this.loginMessage.type = "success";
-    //         this.loginMessage.title = "Account created !";
-    //         this.loginMessage.message = "Redirecting...";
-    //         this.$timeout(() => {
-    //             this.loginMessage = null;
-    //             this.$location.path('/');
-    //         }, 2000);
-    //     }).catch((res) => {
-    //         this.sessionFactory.isLogged = false;
-    //         this.$rootScope.$emit('loginStatusChanged', false);
-    //         this.loginMessage = {};
-    //         this.loginMessage.type = "error";
-    //         this.loginMessage.title = "Sign up error";
-    //         this.loginMessage.message = res.data;
-    //     });
-    // };
-
 }
