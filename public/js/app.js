e56130772c35d7e9041a5816623ea381e4ed8d06
daedalus418 <morin.gaelle@gmail.com>
2017-01-16 16:10:52 +0100

    todo service removed

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,6 @@
 angular.module('app', ['ngRoute'])
         .factory('sessionFactory', sessionFactory)
         .service('userService', userService)
-        .service('todoService', todoService)
         .controller('mainController', mainController)
         .controller('navbarController', navbarController)
         .controller('loginController', loginController)
